7a501bbaa3edbbc5549d5c58a52856ab02d0d86e
Clarissa <you@example.com>
2021-06-14 20:54:31 -0300

    resolucao bug

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 
 const express = require("express");
-// const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 const routes = require("./routes/router");
 app.use(express.json());
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (request, response) => {
   return response.json({ message: "Server is up" });
