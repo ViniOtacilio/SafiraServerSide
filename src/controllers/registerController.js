8c04ced6b629170b30d860f397143eeee2586397
Clarissa <you@example.com>
2021-06-14 20:54:31 -0300

    resolucao bug

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,10 +1,10 @@
 const { registerService } = require('../services/registerService');
 
-const { createNewUser } = registerService;
+const { createNewUser } = require('../services/registerService');
 
 
 const createUser = async (req, res, next) => {
-    const { name, email, password, passwordRepeated } = request.body;
+    const { name, email, password, passwordRepeated } = req.body;
 
     try {
         await createNewUser(name, email, password, passwordRepeated);
@@ -18,5 +18,5 @@ const createUser = async (req, res, next) => {
 }
 
 module.exports = {
-  createUser
+    createUser
 }
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const { registerController } = require('../controllers/registerController');
+const { createUser } = require('../controllers/registerController');
+
 
 const router = express.Router();
 
-router.post('/users/register', registerController.createUser);
+router.post('/users/register', createUser);
 
-module.exports = router
+module.exports = router;
