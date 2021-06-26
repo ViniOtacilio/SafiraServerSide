759fea3a48df4a54cef9c6ca644cb5440882ade7
Artur Machado <arturmachado100@gmail.com>
2021-06-26 18:40:25 -0300

    Add authenticationMiddleware to protect the routes from users without auth

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,34 +2,15 @@
 const express = require("express");
 const app = express();
 const passport = require("passport");
-const initializePassport = require("../utils/passportConfig.js");
-const session = require("express-session");
 
-initializePassport(passport);
-
-app.use(
-    session({
-        // Key we want to keep secret which will encrypt all of our information
-        secret: process.env.SESSION_SECRET,
-        // Should we resave our session variables if nothing has changes which we dont
-        resave: false,
-        // Save empty value if there is no vaue which we do not want to do
-        saveUninitialized: false
+app.post("/login",
+    passport.authenticate("local", { 
+        successRedirect: '/', 
+        failureRedirect: '/login?fail=true' 
     })
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-/*
-app.post(
-    '/login',
-    passport.authenticate("local", { failureRedirect: '/login' }),
-    function (req, res) {
-        res.redirect('/');
-    }
-);
+)
 
 
-module.exports = {
-    loginUser
-}*/
+app.get('/login',  (request, response ) => {
+    return response.json({ message: "login" });
+})
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 const passport = require("passport");
 const express = require("express");
-const app = express();
 const routes = require("./routes/router");
 const session = require("express-session");
+const indexRouter = require('./routes/home');
+
+function authenticationMiddleware(request, response, next) {
+    if (request.isAuthenticated()) return next();
+    response.redirect('/login?fail=true');
+}
+  
+const app = express();
 
 app.use(express.json());
 
-
-app.get("/", (request, response) => {
-    return response.json({ message: "Server is up" });
+/*
+app.get('/', authenticationMiddleware, (request, response, next) => {
+    response.json({ message: "Server is up" });
     console.log("conectado ao banco");
 });
+*/
 
 app.use("/api", routes);
 
 //LOGIN
-app.use(passport.initialize());
-app.use(passport.session());
-
+require('./utils/passportConfig')(passport);
 app.use(
     session({
         // Key we want to keep secret which will encrypt all of our information
@@ -29,15 +35,23 @@ app.use(
         saveUninitialized: false
     })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.post(
     '/login',
-    passport.authenticate("local"),
-    function (req, res) {
-        res.redirect('/');
-    }
+    passport.authenticate("local", { 
+        successRedirect: '/', 
+        failureRedirect: '/login?fail=true' 
+    })
 );
+
+app.get('/login',  (request, response ) => {
+    return response.json({ message: "login" });
+})
 //LOGIN
+
+app.use('/', authenticationMiddleware,  indexRouter);
 
 app.listen(3333);
 
