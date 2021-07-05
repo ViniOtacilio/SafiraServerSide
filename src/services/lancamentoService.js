0a298488c3290269f3c405bb7b7c6dd7d1e709e4
Vinicius Otacilio <viniotacilio@gmail.com>
2021-07-05 10:46:27 -0300

    Organizando as queries

--- src/services/lancamentoService.js
+++ src/services/lancamentoService.js
@@ -1,28 +1,34 @@
-const { pool } = require('../database.js');
+const registerNewLancamentoQuery = require("../model/lancamentoUserQuery");
 
-const createNewLancamento = async (value, tipo_de_transacao, userid, categoriaid, titulo_lancamento, data_lancamento) => {
-   
-    let errors = [];
+const createNewLancamento = async (
+  value,
+  tipo_de_transacao,
+  userid,
+  categoriaid,
+  titulo_lancamento,
+  data_lancamento
+) => {
+  let errors = [];
 
-    if (!value || !tipo_de_transacao || !categoriaid || !titulo_lancamento) {
-        errors.push({ message: "Por favor preencha todos os campos!" });
-    }
+  if (!value || !tipo_de_transacao || !categoriaid || !titulo_lancamento) {
+    errors.push({ message: "Por favor preencha todos os campos!" });
+  }
 
-    if (errors.length > 0) {
-        throw errors;
-    } else {
-        pool.query(
-            `INSERT INTO lancamentos (value, tipo_de_transacao, userid, categoriaid, titulo_lancamento, data_lancamento, data_ultima_alteracao)
-       VALUES ($1, $2, $3, $4, $5, $6, current_timestamp)`, [value, tipo_de_transacao, userid, categoriaid, titulo_lancamento, data_lancamento], (err, result) => {
-                if (err) {
-                throw err
-                
-            }
-          }
-        )
-    }
-}
+  if (errors.length > 0) {
+    throw errors;
+  } else {
+    //Criando novo lançamento no banco de dados
+    registerNewLancamentoQuery.createNewLancamentoQuery(
+      value,
+      tipo_de_transacao,
+      userid,
+      categoriaid,
+      titulo_lancamento,
+      data_lancamento
+    );
+  }
+};
 
-    module.exports = {
-        createNewLancamento
-    }
+module.exports = {
+  createNewLancamento,
+};
