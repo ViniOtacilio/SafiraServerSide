99c2c3703ae9ccc9ae6fd0c551a35e90b6a0edb3
Artur Machado <arturmachado100@gmail.com>
2021-07-12 19:03:28 -0300

    Corrigido API de lançamentos para estar nos mesmos arquivos do create lancamento

--- src/controllers/lancamentoController.js
+++ src/controllers/lancamentoController.js
@@ -1,5 +1,5 @@
 const { createNewLancamento } = require('../services/lancamentoService'); 
-
+const { getLancamentoByUser } = require('../services/lancamentoService'); 
 
 const createLancamento = async (req, res, next) => {
 
@@ -14,8 +14,35 @@ const createLancamento = async (req, res, next) => {
     catch (e) {
         res.sendStatus(500);
     }
-}
+};
+
+const getLancamento = async (req, res, next) => {
+
+    const user_id = req.query.user_id;
+    const id = req.query.id;
+    const status = req.query.status;
+    const titulo = req.query.titulo;
+    const start_date = req.query.start_date;
+    const end_date = req.query.end_date;
+
+    console.log("Dentro do Controller de getlancamento:" + user_id + "-" + id + "-" + status +  "-" + titulo + "-" + start_date + "-" + end_date);
+
+    try {
+        result = await getLancamentoByUser( user_id, id, status, titulo, start_date, end_date)
+        console.log('controller:');
+        console.log(result);
+        res.send(result);
+        next();
+    }
+    catch (e) { 
+        console.log(e)
+        res.send({
+            message:e
+         });
+    }
+};
 
 module.exports = {
-    createLancamento
+    createLancamento,
+    getLancamento
 }
--- src/model/lancamentoUserQuery.js
+++ src/model/lancamentoUserQuery.js
@@ -27,6 +27,22 @@ const createNewLancamentoQuery = async (
   );
 };
 
+const getLancamentoQuery = async (query) => {
+    return new Promise(function(resolve, reject) {
+        console.log('In model: '+ query);
+
+         pool.query(query, (err, result) => {
+            if (err) {
+                throw (err) ;
+            }
+         console.log(result.rows);
+            resolve (result.rows) ;
+            })
+       
+        })        
+};
+
 module.exports = {
   createNewLancamentoQuery,
+  getLancamentoQuery
 };
