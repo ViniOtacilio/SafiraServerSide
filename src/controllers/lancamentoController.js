d78cde73a6900330ac12451059fe5fdddc579a09
Clarissa <you@example.com>
2021-07-16 12:52:00 -0300

    adicionar comentario no lancamento

--- src/controllers/lancamentoController.js
+++ src/controllers/lancamentoController.js
@@ -3,11 +3,11 @@ const { getLancamentoByUser } = require('../services/lancamentoService');
 
 const createLancamento = async (req, res, next) => {
 
-    const { value, tipo_de_transacao, categoriaid, titulo_lancamento, data_lancamento } = req.body;
+    const { value, tipo_de_transacao, categoriaid, titulo_lancamento, data_lancamento, comentario } = req.body;
     const userid = req.user.user_id;
 
     try {
-        await createNewLancamento(value, tipo_de_transacao, userid, categoriaid, titulo_lancamento, data_lancamento);
+        await createNewLancamento(value, tipo_de_transacao, userid, categoriaid, titulo_lancamento, data_lancamento, comentario);
         res.sendStatus(201);
         next();
     }
