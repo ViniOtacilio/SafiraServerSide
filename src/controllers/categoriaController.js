3af2521335101d70d13470137ec0544770ed41cf
Vinicius Otacilio <viniotacilio@gmail.com>
2021-07-16 19:22:51 -0300

    Alterando categoria e alterando requisicao de lancamento

--- src/controllers/categoriaController.js
+++ src/controllers/categoriaController.js
@@ -1,9 +1,10 @@
 const { getCategories } = require('../services/categoriaService'); 
 
 
-const getAllCategories = async (req, res, next) => {
+const getAllCategories = async(req, res, next) => {
     try {
         let categories = await getCategories();
+        console.log(categories);
         // res.send(categories);
         res.sendStatus(200).send(categories)
         // res.sendStatus(201);
--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -4,26 +4,25 @@ const { createNewUser } = require('../services/registerService');
 
 const createUser =  (req, res, next) => {
     const { name, email, password, repeatedPassword } = req.body;
+    let loginUser =  setTimeout(function () {
+        passport.authenticate("local", function (err, user, info) {
+            if (err) {
+                return next(err);
+            }
+            if (!user) {
+                return res.sendStatus(500);
+            }
+            req.login(user, function (error) {
+                if (error) return next(error);
+                console.log("Request Login supossedly successful " + req.isAuthenticated() + req.user.user_id);
+            });
+            // res.redirect('/');
+            createSaldo(req.user.user_id);
+        })(req, res, next);
+    }, 3000);
 
     try {
-        createNewUser(name, email, password, repeatedPassword);
-        setTimeout(function () {
-            passport.authenticate("local", function (err, user, info) {
-                if (err) {
-                    return next(err);
-                }
-                if (!user) {
-                    return res.sendStatus(500);
-                }
-                req.login(user, function (error) {
-                    if (error) return next(error);
-                    console.log("Request Login supossedly successful " + req.isAuthenticated() + req.user.user_id);
-                });
-                res.redirect('/');
-                createSaldo(req.user.user_id);
-            })(req, res, next);
-        }, 3000);
-        
+        createNewUser(name, email, password, repeatedPassword).then(loginUser);
     }
     catch(e) {
         res.sendStatus(500);
