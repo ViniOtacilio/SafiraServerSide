e1c22c805b9480d507af5d4be3f8cb1ab20490e0
Artur Machado <arturmachado100@gmail.com>
2021-08-06 21:51:21 -0300

    API de categorias criada

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,6 +4,7 @@ const { createLancamento } = require('../controllers/lancamentoController');
 const { getLancamento } = require('../controllers/lancamentoController');
 const { createSaldo } = require('../controllers/saldoController');
 const { getAllCategories } = require('../controllers/categoriaController');
+const { getCategoria } = require('../controllers/categoriaController');
 const { getSaldo } = require('../controllers/saldoController');
 
 
@@ -14,6 +15,7 @@ router.post('/users/novoLancamento', createLancamento);
 router.get('/users/lancamento', getLancamento);
 //router.post('/novoSaldo', createSaldo);
 router.get('/categorias', getAllCategories)
+router.get('/users/categoria', getCategoria)
 router.get('/users/saldo', getSaldo);
 
 module.exports = router;
